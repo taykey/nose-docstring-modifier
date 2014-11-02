d784b83b91cdadc5ab6e5e8088e5dc41d8ec5f22
Maroun Maroun <maroun@taykey.com>
2014-11-02 11:53:03 +0200

    Made a class member a variablea

--- docstring_plugin/docstring_plugin.py
+++ docstring_plugin/docstring_plugin.py
@@ -21,12 +21,12 @@ class DocString(Plugin):
     def describeTest(self, running_test):
 
         # meta information about running test
-        self.test = running_test.test.__dict__['test']
+        test = running_test.test.__dict__['test']
 
-        prefix = self._get_affix('prefix')
-        suffix = self._get_affix('suffix')
+        prefix = self._get_affix('prefix', test)
+        suffix = self._get_affix('suffix', test)
 
-        docstring = self._get_docstring()
+        docstring = self._get_docstring(test)
 
         return '({}) {} ({})'.format(prefix, docstring, suffix)
 
@@ -55,28 +55,28 @@ class DocString(Plugin):
         if options.replace:
             self.args['replace'] = options.replace.split(',')
 
-    def _get_affix(self, affix_type):
+    def _get_affix(self, affix_type, running_test):
         """
         Returns list containing affixes that should be appendind to docstring.
         :param affix_type: 'suffix' or 'prefix'
         :type affix_type: str
-        :param keywords: contains meta information about running test
+        :param running_test: contains meta information about the current test
         :return: a list containing wanted affixes depending on 'affix_type'
         """
         affix = list()
 
         for key in self.args[affix_type]:
-            affix.append(self.test.keywords.get(key, ''))
+            affix.append(running_test.keywords.get(key, ''))
 
         # remove empty strings resulted from unexisting keys
         affix = filter(len, map(str, affix))
         return ', '.join(affix)
 
-    def _get_docstring(self):
+    def _get_docstring(self, running_test):
         """
         :return: modified docstring if --replace is toggled, original otherwise.
         """
-        docstring = self.test.__doc__
+        docstring = running_test.__doc__
         if 'replace' in self.args.keys() and len(self.args['replace']) == 2:
             return docstring.replace(self.args['replace'][0],
                                      self.args['replace'][1])
